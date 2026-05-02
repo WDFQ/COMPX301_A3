db58b6519ed036bd61f4503882a96f506dc63807
KESPC <alexismanosca@gmail.com>
2026-05-02 15:12:02 +1200

    added new files

---
+++ LZ78decode.java
@@ -0,0 +1,9 @@
+public class LZ78decode {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+            System.err.println("An error has occurred: " + e);
+        }
+    }
+}
